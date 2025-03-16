2df15a6efbcf76cb29d0189c50219093e55e96d4
yunaie <sirineyk@outlook.de>
2025-03-16 20:32:21 +0100

    service api func fetch

---
+++ VendeurView.swift
@@ -0,0 +1,20 @@
+//
+//  VendeurView.swift
+//  LaCaveAJeu
+//
+//  Created by etud on 16/03/2025.
+//
+
+import SwiftUI
+
+struct VendeurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VendeurView_Previews: PreviewProvider {
+    static var previews: some View {
+        VendeurView()
+    }
+}
