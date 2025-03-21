038a2b36fa62c179730ef336729b6d710204ab74
yunaie <sirineyk@outlook.de>
2025-03-21 11:34:00 +0100

    start of vente

--- LaCaveAJeu/View/VentesView.swift
+++ LaCaveAJeu/View/VentesView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct VentesView: View {
+    @StateObject private var viewModelJeux = JeuxViewModel()
+    @StateObject private var viewModelVentes = VendeurlViewModel()
+    let bleuclair = Color(red:121/255.0, green :178/255.0,blue: 218/255.0)
+    let yellowlight = Color(red : 240/255.0,green: 230/255.0, blue:158/255.0)
+    let bleufonce = Color(red: 45/255.0, green: 85/255.0,blue: 166/255.0)
+    let bleutresclair = Color(red: 216/255.0, green: 239/255.0,blue: 255/255.0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer().frame(height: 80);
+            HStack{
+                Spacer()
+                Text("💸")
+                    .font(.system(size:40))
+                    .padding(.trailing, 30.0)
+                
+            }
+            HStack{
+                Spacer()
+                Text("Faire une vente")
+                    .font(.jomhuriaBigger())
+                    .foregroundColor(bleuclair)
+                Spacer()
+            }
+            Spacer().frame(height: 10);
+            VStack{
+                ScrollView{
+                    
+                }
+            }.frame(width: 340, height: 600)
+                .background(bleufonce)
+                .cornerRadius(10)
+                .padding(.horizontal, 30.0)
+                .shadow(radius: 6)
+                .onAppear {
+                    viewModelVentes.fetchVendeurs()
+                }
+        }.background(yellowlight)
+            .edgesIgnoringSafeArea(.all)
+        
+        
     }
 }
 
