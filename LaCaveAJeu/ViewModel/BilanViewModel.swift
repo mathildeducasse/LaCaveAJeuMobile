c6438b5344976dce43534fe9e3b643009b9d05aa
yunaie <sirineyk@outlook.de>
2025-03-26 01:57:05 +0100

    done

--- LaCaveAJeu/ViewModel/BilanViewModel.swift
+++ LaCaveAJeu/ViewModel/BilanViewModel.swift
@@ -3,73 +3,96 @@ import SwiftUI
 class BilanViewModel: ObservableObject {
     private let apiService = APIService.shared
     private let transactionService = ServiceTransaction.shared
+    private let vendeurService = APIService()
     
     @Published var bilan: BilanVendeur?
     
-    func fetchBilan(for vendeurId: String) {
-        guard !vendeurId.isEmpty else {
-            self.bilan = nil
-            return
-        }
-        
-        var nbDepots = 0
-        var nbVentes = 0
-        var portefeuilleTotal = 0.0
-        var fraisTotal = 0.0
-        var jeuxVendus = 0
-        
-        transactionService.fetchFilteredTransaction(
-            gestionnaire: nil,
-            acheteur: nil,
-            proprietaire: vendeurId,
-            prix_min: nil,
-            prix_max: nil,
-            remise: nil,
-            nameJeu: nil,
-            statut: nil
-        ) { transactions in
+        func fetchBilan(for vendeurId: String) {
+            guard !vendeurId.isEmpty else {
+                self.bilan = nil
+                return
+            }
             
-            for transac in transactions {
-                if transac.statut == "depot" {
-                    nbDepots += 1
-                    fraisTotal += Double(transac.frais - transac.remise)
-                } else if transac.statut == "vente" {
-                    nbVentes += 1
-                    for jeu in transac.jeux {
-                        // On filtre les jeux appartenant à ce vendeur
-                        if jeu.vendeur == vendeurId {
-                            jeuxVendus += jeu.quantite
-                            portefeuilleTotal += Double(jeu.quantite) * jeu.prix_unitaire
-                            
+            var nbDepots = 0
+            var nbVentes = 0
+            var fraisTotal = 0.0
+            var jeuxVendus = 0
+            var jeuxRestants = 0
+            var portefeuilleTotal = 0.0
+
+            let group = DispatchGroup()
+
+            vendeurService.fetchVendeurs { vendeurs in
+                guard let vendeur = vendeurs.first(where: { $0.id == vendeurId }) else {
+                    DispatchQueue.main.async { self.bilan = nil }
+                    return
+                }
+
+                portefeuilleTotal = vendeur.soldes
+
+                // 1. VENTES
+                group.enter()
+                self.transactionService.fetchFilteredTransaction(
+                    gestionnaire: nil, acheteur: nil, proprietaire: nil,
+                    prix_min: nil, prix_max: nil, remise: nil,
+                    nameJeu: nil, statut: "vente"
+                ) { transactions in
+                    for transaction in transactions {
+                        var prop = false
+                        for jeu in transaction.jeux {
+                            if jeu.vendeur == vendeur.nom {
+                                prop = true
+                                jeuxVendus += jeu.quantite
+                            }
+                        }
+                        if prop {
+                            nbVentes += 1
                         }
                     }
+                    group.leave()
                 }
-                
-                // Maintenant on récupère les jeux restants
+
+                // 2. DEPOTS
+                group.enter()
+                self.transactionService.fetchFilteredTransaction(
+                    gestionnaire: nil, acheteur: nil, proprietaire: vendeur.id,
+                    prix_min: nil, prix_max: nil, remise: nil,
+                    nameJeu: nil, statut: "depot"
+                ) { transactions in
+                    for transaction in transactions {
+                        nbDepots += 1
+                        fraisTotal += Double(transaction.frais - transaction.remise)
+                    }
+                    group.leave()
+                }
+
+                // 3. JEUX RESTANTS
+                group.enter()
                 self.apiService.fetchFilteredGames(
-                    proprietaire: vendeurId,
-                    prix_min: nil,
-                    prix_max: nil,
-                    categorie: [],
-                    intitule: nil,
-                    statut: "disponible",
-                    editeur: nil,
+                    proprietaire: vendeur.id,
+                    prix_min: nil, prix_max: nil,
+                    categorie: [], intitule: nil,
+                    statut: "disponible", editeur: nil,
                     quantites: nil
                 ) { jeux in
-                    let jeuxRestants = jeux.reduce(0) { $0 + $1.quantites }
-                    
-                    DispatchQueue.main.async {
-                        self.bilan = BilanVendeur(
-                            nbDepots: nbDepots,
-                            nbVentes: nbVentes,
-                            portefeuilleTotal: portefeuilleTotal,
-                            fraisTotal: fraisTotal,
-                            jeuxRestants: jeuxRestants,
-                            jeuxVendus: jeuxVendus
-                        )
-                    }
+                    jeuxRestants = jeux.reduce(0) { $0 + $1.quantites }
+                    group.leave()
+                }
+
+                // Quand les 3 appels sont terminés
+                group.notify(queue: .main) {
+                    self.bilan = BilanVendeur(
+                        nbDepots: nbDepots,
+                        nbVentes: nbVentes,
+                        portefeuilleTotal: portefeuilleTotal,
+                        fraisTotal: fraisTotal,
+                        jeuxRestants: jeuxRestants,
+                        jeuxVendus: jeuxVendus
+                    )
                 }
             }
         }
+
     }
-}
+
+   
